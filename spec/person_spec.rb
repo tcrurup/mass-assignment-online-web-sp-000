09118e33b71e8f0b687c7d584c92fce8a65abc32
Anthony Rurup <tcrurup@gmail.com>
2019-05-03 06:49:32 +0000

    Automatically backed up by Learn

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/person'
 # If you write a describe block without quotes like this, you need to define
 # the described class (or method) for the tests to even run. It's sort of a
 # freebie test to make sure you've actually defined the models you plan on
